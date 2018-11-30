9231816e1f1c1fcb8cf677ca220a7bab9aed9700
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2018-12-01 00:50:01 +0100

    publish

---
+++ README.md
@@ -0,0 +1,61 @@
+# gdrive-auditor
+
+A tool that gives you a birds-eye view over sharing settings in Google Drive folders. This project is not feature complete in any way, so consider it a source of inspiration rather than a polished tool. Feel free to contribute features & ideas. PRs are welcome!
+
+## Example usage
+
+![Alt text](public/gdrive-auditor.png?raw=true "Example usage")
+
+In this example, the yellow files and folders are shared with person.a@mydomain.com and person.c@mydomain.com. Folder1 and Folder2 are printed in white and without annotated email addresses, meaning they are not shared with anyone.
+
+## Setup
+
+### Prerequisites
+
+* You need to have Clojure & [Leiningen](https://leiningen.org/) installed
+
+### Authentication
+
+To give the tool access to your Drive files you need to create an OAuth client ID in Google Cloud and authorize using your user account.
+
+1. Using the Google [Developer's Console](https://console.developers.google.com/)
+2. Select an existing project, or create a new one if needed
+3. Enable the Google Drive API in the [API Library](https://console.developers.google.com/apis/library)
+4. Create a new credential of type "OAth client ID" on the [Credentials page](https://console.developers.google.com/apis/credentials)
+5. Using information from the credential, create the file `config/google-creds.edn` using the following template:
+
+```edn
+{:client-id "MY_CLIENT_ID"
+ :client-secret "MY_CLIENT_SECRET"
+ :redirect-uris ["https://localhost"]
+ :auth-map {}}
+```
+
+6. Run the login command
+`lein run login`
+
+7. Open the printed URL in a browser and go through the authorization flow
+8. Copy the resulting code and paste it into the terminal, followed by enter. The auth tokens will be automatically written to the `:auth-map` key in the `config/google-creds.edn` file
+
+9. Rename the file `config/example-defaults.edn` to `config/defaults.edn` and add your email address, so that it can be filtered out when visualizing Drive collaborators.
+
+## Usage
+
+Run the tree command with a Google Drive folder ID as an argument
+
+`lein run tree MY_FOLDER_ID`
+
+The folder ID is the last segment of a Google Drive URL. 
+Example `https://drive.google.com/drive/u/0/folders/MY_FOLDER_ID`
+
+## TODO & Ideas (PRs welcome!)
+
+* Add support for JSON output
+
+## License
+
+Copyright © 2018 Jonathan Pettersson
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
+ 
---
+++ src/gdrive_auditor/core.clj
@@ -0,0 +1,92 @@
+(ns gdrive-auditor.core
+  (:use [clojure.pprint])
+  (:use [clojure.walk])
+  (:require [clojure.data.json :as json])
+  (:require [google-apps-clj.google-drive :as gdrive])
+  (:require [google-apps-clj.credentials :as gauth])
+  (:require [clojure.term.colors :refer :all])
+  (:require [clojure.edn :as edn])
+  (:gen-class))
+
+(def me ((edn/read-string (slurp "config/defaults.edn")) :exclude-user))
+(def creds-file-location "secrets/google-creds.edn")
+
+(defn read-directory
+  [creds path]
+  (map
+   #(hash-map 
+     :id (% :id)
+     :title (% :title)
+     :shared (% :shared)
+     :mime-type (% :mime-type))
+   (gdrive/list-files! creds path)))
+
+(defn get-collaborators
+  [creds file]
+  (if (file :shared)
+    (remove #(= % me)
+            (map #(% :email-address)
+                 (gdrive/get-permissions! creds (file :id))))))
+
+(defn is-directory?
+  [file]
+  (= (file :mime-type) "application/vnd.google-apps.folder"))
+
+(defn print-file
+  [file collaborators depth]
+  (let [lb (if (is-directory? file) "[ " "")
+        rb (if (is-directory? file) " ]" "")]
+    (println
+     (apply str (repeat depth "\t"))
+     (if (file :shared)
+       (format "%s%s%s %s"
+               (yellow (on-grey (bold lb)))
+               (yellow (on-grey (bold (file :title))))
+               (yellow (on-grey (bold rb)))
+               (yellow (pr-str collaborators)))
+       (format "%s%s%s"
+               lb
+               (file :title)
+               rb)))))
+
+(defn walk-tree
+  ([creds path]
+   (walk-tree creds path 0))
+  ([creds path depth]
+   (let [files (read-directory creds path)]
+     (doall (for [file files]
+              (let [collaborators (get-collaborators creds file)]
+                (if (is-directory? file)
+                  (do
+                    (print-file file collaborators depth)
+                    (walk-tree creds (file :id) (+ depth 1)))
+                  (print-file file collaborators depth))))))))
+
+(defn persist-auth-map
+  [creds auth-map]
+  (let [new-creds (conj creds [:auth-map auth-map])]
+    (spit creds-file-location (pr-str new-creds))))
+
+(defn login
+  []
+  (let [creds (edn/read-string (slurp creds-file-location))
+        response (google-apps-clj.credentials/get-auth-map creds ["https://www.googleapis.com/auth/drive"])
+        auth-map (keywordize-keys
+                 (into (sorted-map)
+                       (map (fn [[key val]] [(clojure.string/replace key #"_" "-") val])
+                            (json/read-str (.toString response)))))]
+       (do (persist-auth-map creds auth-map)
+           (println (format "Authentication token written to ./%s" creds-file-location))
+           (println "Login successful!"))))
+
+(defn tree
+  [directory-id]
+  (let [creds (edn/read-string (slurp creds-file-location))]
+    (do
+      (walk-tree creds directory-id)
+      "")))
+
+(defn -main [& args]
+   (case (first args)
+    "login" (login)
+    "tree" (tree (nth args 1))))
